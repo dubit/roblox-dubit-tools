1d5b32459553c2c9c28cb30b7d044e44f73756c2
Jakub Piłasiewicz <kubapilasiewicz60@gmail.com>
2025-12-11 13:39:20 +0100

    Remove UGCToolkit and EnvironmentTool packages

--- Packages/EnvironmentTool/Tests/TestRunner.server.lua
+++
@@ -1,9 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local TestEz = require(ReplicatedStorage.DevPackages.TestEz)
-
-print("[TestRunner]: TestEZ Running, please be patient if you're running tests on a LIVE environment.")
-
-TestEz.TestBootstrap:run({
-	ReplicatedStorage.Packages,
-})
--- Packages/UGCToolkit/Tests/Source/Development.server.lua
+++
@@ -1,4 +0,0 @@
---[[
-	Development script - this script is not part of the package, and should only be used for testing the functionality
-	of the package within the Roblox engine.
-]]
